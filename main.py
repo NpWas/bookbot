bc86800d953f6d09b27d92740fc8c0b8342e46af
npwas <npwashington@gmail.com>
2024-08-17 00:59:32 -0400

    added count characters function

--- main.py
+++ main.py
@@ -14,15 +14,27 @@ def count_words(file_contents):
 
 
 
-#def count_characters():
+def count_characters(file_contents):
+    character_list = list(file_contents.lower())
+    
+    char_counter = 0
 
+    char_dict = {}
+
+    for char in character_list:
+        char_counter += 1
+        char_dict[char] = char_counter
+
+    return char_dict
 
 
 def main():
-    count_words(frankenstein)  
+    
+     
   
 
     print(f"There are {count_words(frankenstein)} in frankenstein")
+    print(count_characters(frankenstein))
 
 if __name__ == "__main__":
     main()                                
